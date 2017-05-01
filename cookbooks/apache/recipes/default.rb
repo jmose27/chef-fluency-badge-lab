8b0566a958b7375703d092bcd0e404dedb7ee0d3
jmose27 <james.moseley@synctree.com>
2017-05-01 20:45:29 +0000

    added motd and changed to install on both centos and ubuntu

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,12 +5,19 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 #install apache package for centos
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == 'debian'
+	package = 'apache2'
+end
 package 'apache2' do
-	package_name 'httpd'
+	package_name pakage
 	action :install
 end
 
 service 'apache2' do 
-	service_name 'httpd'
+	service_name package
 	action [:start, :enable]
 end
+
+include_recipe 'apache::websites'
